a96d6b27c9c5faebda64773780e57bb25fc35171
radsakk <lherr019@jaguar.tamu.edu>
2023-07-04 03:16:19 -0500

    3rd commit

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -5,3 +5,31 @@ function buttonNum(num) {
 function clearResult() {
     document.getElementById("result").value = ""
 }
+
+function calculateResult() {
+    let num = document.getElementById("result").value
+    let result = eval(num)
+
+    document.getElementById("result").value = result
+}
+
+function plusMinus() {
+    let num = document.getElementById("result").value
+    let result = eval(num * -1)
+
+    document.getElementById("result").value = result
+}
+
+function percentage() {
+    let num = document.getElementById("result").value
+    let result = eval(num / 100)
+
+    document.getElementById("result").value = result
+}
+
+function exponent() {
+    let num = document.getElementById("result").value
+    let result = eval(num ** num)
+
+    document.getElementById("result").value = result
+}
